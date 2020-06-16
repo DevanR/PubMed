396e4607bea8b7e300b62dbdeab34418a592c37d
Devan <rehunathan@gmail.com>
2020-06-17 00:13:14 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import json
+from datetime import datetime
+
+import pubmed_parser as pp
+from Bio import Entrez
+from openpyxl import load_workbook
+
+EMAIL = 'your.email@example.com'
+FILE_NAME = 'input.xlsx'
+
+def search(query):
+    Entrez.email = EMAIL
+    handle = Entrez.esearch(db='pubmed',
+                            sort='pub+date',
+                            retmax='100',
+                            retmode='xml',
+                            mindate='2020/01/01',
+                            maxdate=datetime.today().strftime('%Y/%m/%d'),
+                            datetype='pdat',
+                            term=query)
+    results = Entrez.read(handle)
+    return results
+
+def fetch_details(id_list):
+    ids = ','.join(id_list)
+    Entrez.email = EMAIL
+    handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+    results = Entrez.read(handle)
+    return results
+
+def read_authors(file_name):
+    wb = load_workbook(file_name)
+
+    author_ws = wb['List 1_by names']
+    author_list = []
+    for i in range(1, author_ws.max_row+1):
+        if isinstance(author_ws.cell(row=i, column=1).value, int):
+            author_list.append(author_ws.cell(row=i, column=4).value+'[Author]')
+
+    return author_list
+
+def read_institutes(file_name):
+    wb = load_workbook(file_name)
+
+    institute_ws = wb['List 2_by Institute']
+    institute_list = []
+
+    for i in range(1, institute_ws.max_row+1):
+        if isinstance(institute_ws.cell(row=i, column=1).value, int):
+            institute_list.append(institute_ws.cell(row=i, column=2).value+'[Affiliation]')
+
+    return institute_list
+
+if __name__ == '__main__':
+
+    authors = read_authors(FILE_NAME)
+    institutes = read_institutes(FILE_NAME)
+
+    # Get Authors
+    #results = search(' OR '.join(authors))
+
+    # Get Organisations
+    results = search(' OR '.join(institutes))
+
+    if results['IdList']:
+        id_list = results['IdList']
+        papers = fetch_details(id_list)
+
+        for i, paper in enumerate(papers['PubmedArticle']):
+
+            import pdb
+            pdb.set_trace()
+
+            #print(json.dumps(paper, indent=2))
+
+            #print(json.dumps(papers[0], indent=2, separators=(',', ':')))
+    else:
+        print('Nothing found!')
